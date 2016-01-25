5c5128a208d2394b1c8ea04ccd2d028e7681d725
Cameron Hunter <chunter@twitter.com>
2016-01-24 18:17:21 -0800

    Extract server logic out to bin/server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,12 @@
-import Express from "express";
-import compression from "compression";
-import path from "path";
-
 import React from "react";
 import ReactDOM from "react-dom/server";
-import { match, RouterContext } from "react-router";
+import { RouterContext, match } from "react-router";
 
 import App from "./containers/App";
 import routes from "./routes";
 
-const app = new Express();
-
-app.use(compression());
-app.use(Express.static(path.join(__dirname, "..", "static")));
-
-app.use((request, response, next) => {
-  match({ routes: routes, location: request.url }, (error, redirectLocation, renderProps) => {
+export default (request, response) => {
+  match({ routes, location: request.url }, (error, redirectLocation, renderProps) => {
     if (error) {
       return response.send(500, error.message);
     }
@@ -36,6 +27,4 @@ app.use((request, response, next) => {
 
     response.status(200).send(`<!doctype html>\n${html}`);
   });
-});
-
-export default app;
+}
